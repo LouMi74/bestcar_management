d83bfa79cb1b86abf5f221016da5ab0f92841b30
LouMi74 <Vonaika@gmail.com>
2026-01-31 15:28:40 +0100

    Merge dev_farid - Farid's contributions

---
+++ bestcar_commercial/models/sale_order.py
@@ -0,0 +1,20 @@
+from odoo import models
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _action_confirm(self):
+        res = super()._action_confirm()
+        for order in self:
+            for line in order.order_line:
+                if line.product_template_id.is_vehicle:
+                    line.product_template_id.status="reserved"
+        return res
+
+    def action_cancel(self):
+        res = super().action_cancel()
+        for order in self:
+            for line in order.order_line:
+                if line.product_template_id.is_vehicle:
+                    line.product_template_id.status="for_sale"
+        return res
